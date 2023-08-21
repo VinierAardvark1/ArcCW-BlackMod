d77f8c84a80b116fdb9768bc57f1c6379d7fb2d0
VinierAardvark1 <jackprovine@gmail.com>
2023-08-21 10:07:07 -0500

    mid class edits lol
    Cut Shells:
    Tweaked description
    Tweaked stat values
    Added mag size hook
    
    PC Bullets:
    Changed full name from Polymer Coated Bullets to Polymer Coated Rounds
    Tweaked Description
    Tweaked stat values

--- lua/arccw/shared/attachments/ammo_cut_shells.lua
+++ lua/arccw/shared/attachments/ammo_cut_shells.lua
@@ -1,6 +1,6 @@
 att.PrintName = "Cut Shells"
 att.Icon = Material("entities/att/placeholder_blackmod.png")
-att.Description = [[A 12 gauge shell cut in a barely overlapping spiral around the hull, leaving a small portion attached. This simple trick makes the shot more like a slug; When fired, the sliver holding the shell together tears off and sends the wad, shotcup, and said hull bit all flying down the barrel. If done improperly, the contents may spill out into the weapon, causing malfunctions. For this reason, shells are loaded directly into the chamber in non-break action or single-shot weapons.
+att.Description = [[A 12 gauge shell cut in a barely overlapping spiral around the hull, leaving a small portion attached. This simple trick makes the shot more like a slug; When fired, the sliver holding the shell together tears off and sends the wad, shotcup, and said hull bit all flying down the barrel. If done improperly, the contents may spill out into the weapon, causing malfunctions. For this reason, shells are loaded directly into the chamber in non-break action or single-shot weapons to reduce the chance of failures.
 Heh, I remember hearing about these. Great Depression-era trick. Used by hunters when they needed a slug but didn't have or couldn't find any. Never heard of anyone using them nowadays, seeing as slugs are easier to come by, but you tell me how well it works, eh?]]
 att.Desc_Pros = {
 }
@@ -17,8 +17,16 @@ att.Mult_CycleTime = 1.05
 att.Mult_RPM = 1.05
 att.Mult_ReloadTime = 1.05
 
-att.Mult_AccuracyMOA = 0.95
-att.Mult_MalfunctionMean = 0.75
+att.Mult_AccuracyMOA = 0.985
+att.Mult_MalfunctionMean = 0.725
+
+att.Hook_GetCapacity = function(wep, cap)
+    return 1
+end
+
+att.Hook_Compatible = function(wep)
+    if (wep.RegularClipSize or wep.Primary.ClipSize) == 1 then return false end
+end
 
 att.Hook_Compatible = function(wep)
     if !wep:GetIsShotgun() then return false end
--- lua/arccw/shared/attachments/ammo_polymer_coated.lua
+++ lua/arccw/shared/attachments/ammo_polymer_coated.lua
@@ -1,9 +1,10 @@
-att.PrintName = "Polymer Coated Bullets"
+att.PrintName = "Polymer Coated Rounds"
 att.AbbrevName = "PC Bullets"
 att.Icon = Material("entities/att/placeholder_blackmod.png")
-att.Description = [[Cleaner, cheaper, more accurate, and most importantly: purple. These are the four key elements of PC (Polymer Coated) Bullets. The aforementioned polymer coating reportedly reduces friction, lead fouling, and wear inside the barrel, as well as reduce the splash-back on steel targets and reduce smoke.
-Never heard'a these until recently. Saw an article online about how good these things are, so I figured I'd order a box and try it out. Not that bad, honest. Of course, it got my curiosity going, so I... acquired more of the rounds. Unfortunately, they're pistol calibers only. No polymer .338 Lapua Magnums or .50 BMGs for you yet. I'm sure you'll still enjoy this little present, from me to you. Hopefully. Those bastards didn't like seeing me again...]]
+att.Description = [[Cleaner, cheaper, more accurate, and most importantly: purple. These are the four key elements of PC (Polymer Coated) Rounds. The aforementioned polymer coating reportedly reduces friction, lead fouling, and wear inside the barrel, as well as reducing the splash-back on steel targets and less smoke. And of course, they are purple.
+Never heard'a these until recently. Saw an article online about how good these things are, so I figured I'd order a box and try it out. Not that bad, honest. Of course, it got my curiosity going, so I... "acquired" more of the rounds. Unfortunately, they're pistol calibers only. No polymer .338 Lapua Magnums or .50 BMGs for you yet. I'm sure you'll still enjoy this little present, from me to you. Hopefully. Those bastards didn't like seeing me again...]]
 att.Desc_Pros = {
+    "Nice and clean"
 }
 att.Desc_Cons = {
 }
@@ -15,8 +16,8 @@ att.Slot = {"ammo_bullet", "uc_ammo"}
 
 att.Mult_AccuracyMOA = 0.85
 att.Mult_MalfunctionMean = 1.5
-att.Mult_HeatCapacity = 1.25
-att.Mult_HeatDissipation = 1.25
+att.Mult_HeatCapacity = 1.275
+att.Mult_HeatDissipation = 1.275
 
 att.Hook_Compatible = function(wep)
     if wep.Num ~= 1 or (wep.Primary.Ammo ~= "pistol") then return false end
