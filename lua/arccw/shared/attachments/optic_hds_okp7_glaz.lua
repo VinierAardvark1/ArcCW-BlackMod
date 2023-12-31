291906b799570118e0242348bb9db9319034c4b8
VinierAardvark1 <jackprovine@gmail.com>
2023-12-31 04:19:54 -0600

    CUTLASS Tweaks
    Tweaked CUTLASS Optic positioning, made thermal yellow on green.
    Tweaked HDS Flip Optic, removed extra zoom to better suit original purpose.

--- lua/arccw/shared/attachments/optic_hds_okp7_glaz.lua
+++ lua/arccw/shared/attachments/optic_hds_okp7_glaz.lua
@@ -2,9 +2,8 @@ att.PrintName = "HDS Flip Sight w/ OKP-7"
 att.AbbrevName = "HDS Flip"
 att.Icon = Material("entities/att/optic_hds_okp7_glaz.png")
 att.Description = [[Magnified medium-range red-dot with a Thermal Feedback System which can outline long-range marks even through smoke for easier shooting.
-Managed to buy this piece off a Spetsnaz sniper. Says it helps him out in the field when visibility is low. I think the lock to it got damaged in shipping. Had to secure the hinges on the thing, so you can't flip it up and down anymore. Still works like a charm, managed to use it for some deer hunting before I put it up for sale. Made some nice venison stew, if you must know.]]
--- Oh yeah, I should also mention; my guys downstairs added some extra zoom to it, since you'll most likely use this for longer distances than the original operator did.
-att.SortOrder = 120
+Managed to buy this piece off a Spetsnaz sniper. Says it helps him out in the field when visibility is low. I think the lock to it got damaged in shipping, so you can't flip it up and down anymore. Still works like a charm, managed to use it for some deer hunting before I put it up for sale.]]
+att.SortOrder = 999
 att.Desc_Pros = {
 	"autostat.holosight",
     "autostat.zoom",
@@ -29,11 +28,11 @@ att.Mult_SightedSpeedMult = 2
 
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 9.45, 0.98),
+        Pos = Vector(0, 8, 0.98),
         Ang = Angle(0, 0, 0),
-        Magnification = 1.5,
+        Magnification = 1,
 		ScrollFunc = ArcCW.SCROLL_ZOOM,
-        ZoomLevels = 10,
+        ZoomLevels = 2,
         ZoomSound = "arccw_go/fiveseven/fiveseven_slideback.wav",
         Thermal = true,
 		ThermalNoCC = false,
@@ -44,12 +43,12 @@ att.AdditionalSights = {
         HolosightData = {
             Holosight = true,
             ScrollFunc = ArcCW.SCROLL_ZOOM,
-			HolosightMagnificationMin = 1.5,
+			HolosightMagnification = 1.1,
             HolosightMagnificationMin = 1.5,
             HolosightMagnificationMax = 6,
             HolosightReticle = Material("hud/scopes/optic_hds_okp7_glaz.png"),
             HolosightNoFlare = true,
-            HolosightSize = 15,
+            HolosightSize = 25,
             HolosightBlackbox = true,
             Colorable = false,
             HolosightPiece = "models/weapons/atts/mw2r_glaz_okp7_hsp.mdl"
@@ -57,11 +56,11 @@ att.AdditionalSights = {
 		IgnoreExtra = true,
     },
     {
-        Pos = Vector(0, 9.45, 0.98),
+        Pos = Vector(0, 8, 0.98),
         Ang = Angle(0, 0, 0),
-        Magnification = 1.5,
+        Magnification = 1,
 		ScrollFunc = ArcCW.SCROLL_ZOOM,
-        ZoomLevels = 10,
+        ZoomLevels = 2,
         ZoomSound = "arccw_go/fiveseven/fiveseven_slideback.wav",
         Thermal = true,
 		ThermalNoCC = false,
@@ -71,12 +70,12 @@ att.AdditionalSights = {
         ThermalHighlightColor = Color(0, 255, 0),
         HolosightData = {
             Holosight = true,
-			HolosightMagnificationMin = 2,
+			HolosightMagnification = 1.1,
             HolosightMagnificationMin = 1.5,
             HolosightMagnificationMax = 6,
             HolosightReticle = Material("hud/scopes/optic_hds_okp7_glaz_beta.png"),
             HolosightNoFlare = true,
-            HolosightSize = 15,
+            HolosightSize = 25,
             HolosightBlackbox = true,
             Colorable = false,
             HolosightPiece = "models/weapons/atts/mw2r_glaz_okp7_hsp.mdl"
@@ -91,7 +90,7 @@ att.HolosightReticle = Material("hud/scopes/optic_hds_okp7_glaz.png")
 att.HolosightSize = 20
 att.HolosightBone = "holosight"
 att.HolosightPiece = "models/weapons/atts/mw2r_glaz_okp7_hsp.mdl"
-att.HolosightMagnificationMin = 2
+att.HolosightMagnification = 1.1
 att.HolosightMagnificationMin = 1.5
 att.HolosightMagnificationMax = 6
 att.HolosightConstDist = 42
