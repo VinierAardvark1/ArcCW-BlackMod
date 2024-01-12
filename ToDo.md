4d8b77638d31d5e61617a2676961b828949e6bac
VinierAardvark1 <jackprovine@gmail.com>
2024-01-11 22:16:42 -0600

    Sound Compactening
    Compacted all weapon sound lua files into one
    Updated ToDo.md

--- ToDo.md
+++ ToDo.md
@@ -3,9 +3,10 @@ To Do (in no particular order):
 -Give every gun proper shell ejection
 -Fix missing bullet on Tankgewehr
 -Fix missing bullets on Carcano 1891
--Reanimate Wz. 35
+-Reanimate/Model Edit Wz. 35 (reanimate for proper bolt cycling, model edit for bipod usage like Hecate II)
 -Reanimate Stevens 620
 -Reanimate Volkssturmgewehr
+-Make Tankgewehr bipod a separate attachment
 -Maybe reanimate other weapons to a higher quality
 -Figure out how the hell bulk attachment files work and maybe use them to reduce file size
 -Figure out how to make single-load ammo attachments load two shells for break-action weapons (hopefully a more effective method than whats currently used)
---
+++ lua/arccw/shared/sh_blackmod_weapon_sounds.lua
@@ -0,0 +1,724 @@
+-- Boys ATR
+sound.Add({
+    name = "boysatr.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/atr_boys/atr_boys_shoot.wav"
+})
+
+sound.Add({
+    name = "boysatr.firedistant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/atr_boys/atr_boys_shoot_distant.wav"
+})
+
+sound.Add({
+    name = "boysatr.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_boltrelease.wav"
+})
+
+sound.Add({
+    name = "boysatr.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_boltback.wav"
+})
+
+sound.Add({
+    name = "boysatr.boltforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_boltforward.wav"
+})
+
+sound.Add({
+    name = "boysatr.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_boltlatch.wav"
+})
+
+sound.Add({
+    name = "boysatr.magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_magin.wav"
+})
+
+sound.Add({
+    name = "boysatr.magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_magout.wav"
+})
+
+sound.Add({
+    name = "boysatr.maghit",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/atr_boys/atr_boys_maghit.wav"
+})
+
+
+
+
+
+-- Carcano 1891
+sound.Add({
+    name = "carcano1891.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/carcano1891/carcano_fp.wav"
+})
+
+sound.Add({
+    name = "carcano1891.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/carcano1891/m24_distance_fire1.wav"
+})
+
+sound.Add({
+    name = "carcano1891.fire_suppressed",
+    channel = CHAN_AUTO,
+    volume = 0.7,
+    level = 120,
+    sound = "weapons/carcano1891/sg550_suppressed_fire1.wav"
+})
+
+sound.Add({
+    name = "carcano1891.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_boltrelease.wav"
+})
+
+sound.Add({
+    name = "carcano1891.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_boltback.wav"
+})
+
+sound.Add({
+    name = "carcano1891.boltforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_boltforward.wav"
+})
+
+sound.Add({
+    name = "carcano1891.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_boltlatch.wav"
+})
+
+sound.Add({
+    name = "carcano1891.clipin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_clipin.wav"
+})
+
+sound.Add({
+    name = "carcano1891.clipout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_clipout.wav"
+})
+
+sound.Add({
+    name = "carcano1891.clipremove",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_clipremove.wav"
+})
+
+sound.Add({
+    name = "carcano1891.roundsin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_roundsin.wav"
+})
+
+sound.Add({
+    name = "carcano1891.empty",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/carcano1891/carcano_empty.wav"
+})
+
+
+
+
+
+-- Fedorov Avtomat
+sound.Add({
+    name = "fedorov.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = {"weapons/fedorov_avtomat/bar_a1.wav", "weapons/fedorov_avtomat/bar_a2.wav", "weapons/fedorov_avtomat/bar_a3.wav"}
+})
+
+sound.Add({
+    name = "fedorov.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/fedorov_avtomat/bizon-1-distant.wav"
+})
+
+sound.Add({
+    name = "fedorov.fire_suppressed",
+    channel = CHAN_AUTO,
+    volume = 0.7,
+    level = 120,
+    sound = "weapons/fedorov_avtomat/ak74_suppressed_fire1.wav"
+})
+
+sound.Add({
+    name = "fedorov.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/enfield_boltlatch.wav"
+})
+
+sound.Add({
+    name = "fedorov.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/m14_boltrelease.wav"
+})
+
+sound.Add({
+    name = "fedorov.rattle",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/rpk_rattle.wav"
+})
+
+sound.Add({
+    name = "fedorov.magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/stg44_magin.wav"
+})
+
+sound.Add({
+    name = "fedorov.magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/stg44_magout.wav"
+})
+
+sound.Add({
+    name = "fedorov.maghit",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/fedorov_avtomat/stg44_maghit.wav"
+})
+
+
+
+
+
+-- PGM Hecate II
+sound.Add({
+    name = "pgmii.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = {"weapons/pgm_hecateii/ax308_fire_1.wav", "weapons/pgm_hecateii/ax308_fire_2.wav", "weapons/pgm_hecateii/ax308_fire_3.wav"}
+})
+
+sound.Add({
+    name = "pgmii.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/pgm_hecateii/pgm_hecateii_distant.wav"
+})
+
+sound.Add({
+    name = "pgmii.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_boltrelease.wav"
+})
+
+sound.Add({
+    name = "pgmii.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_boltback.wav"
+})
+
+sound.Add({
+    name = "pgmii.boltforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_boltforward.wav"
+})
+
+sound.Add({
+    name = "pgmii.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_boltlatch.wav"
+})
+
+sound.Add({
+    name = "pgmii.magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_magin.wav"
+})
+
+sound.Add({
+    name = "pgmii.magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_magout.wav"
+})
+
+sound.Add({
+    name = "pgmii.magrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_magrelease.wav"
+})
+
+sound.Add({
+    name = "pgmii.rattle",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_rattle.wav"
+})
+
+sound.Add({
+    name = "pgmii.empty",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/pgm_hecateii/ax308_empty.wav"
+})
+
+
+
+
+
+-- Izh-43 Sawed Off (Find out why this doesn't work while the others do)
+sound.Add({
+    name = "izh43.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/izh43_sawnoff/doublebarrel_fire.wav"
+})
+
+sound.Add({
+    name = "izh43.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/izh43_sawnoff/mag7-1-distant.wav"
+})
+
+sound.Add({
+    name = "izh43.draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/doublebarrel_draw.wav"
+})
+
+sound.Add({
+    name = "izh43.holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/doublebarrel_draw.wav"
+})
+
+sound.Add({
+    name = "izh43.empty",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/doublebarrel_empty.wav"
+})
+
+sound.Add({
+    name = "izh43.open",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/breakopen.wav"
+})
+
+sound.Add({
+    name = "izh43.ejectone",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/shelleject1.wav"
+})
+
+sound.Add({
+    name = "izh43.ejectboth",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/shellseject.wav"
+})
+
+sound.Add({
+    name = "izh43.loadfirst",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/shellinsert1.wav"
+})
+
+sound.Add({
+    name = "izh43.loadsecond",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/shellinsert2.wav"
+})
+
+sound.Add({
+    name = "izh43.close",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/izh43_sawnoff/breakclose.wav"
+})
+
+
+
+
+
+-- S&W Model 500
+sound.Add({
+    name = "sw500.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/s&w_500/sw500_shoot.wav"
+})
+
+sound.Add({
+    name = "sw500.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/s&w_500/sw500_shoot_distance.wav"
+})
+
+sound.Add({
+    name = "sw500.cockhammer",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/revolver_cock_hammer.wav"
+})
+
+sound.Add({
+    name = "sw500.empty",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/revolver_empty.wav"
+})
+
+sound.Add({
+    name = "sw500.open",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/sw500_openchamber.wav"
+})
+
+sound.Add({
+    name = "sw500.dump1",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/sw500_dumprounds1.wav"
+})
+
+sound.Add({
+    name = "sw500.dump2",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/sw500_dumprounds2.wav"
+})
+
+sound.Add({
+    name = "sw500.load",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/sw500_roundin.wav"
+})
+
+sound.Add({
+    name = "sw500.close",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/s&w_500/sw500_closechamber.wav"
+})
+
+
+
+
+
+-- Stevens 620
+sound.Add({
+    name = "stevens620.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/stevens620/stevens620_fire.wav"
+})
+
+sound.Add({
+    name = "stevens620.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/stevens620/sawedoff-1-distant.wav"
+})
+
+sound.Add({
+    name = "stevens620.pumpback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/stevens620/stevens620_pumpback.wav"
+})
+
+sound.Add({
+    name = "stevens620.pumpforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/stevens620/stevens620_pumpforward.wav"
+})
+
+sound.Add({
+    name = "stevens620.shellinsert",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/stevens620/stevens620_shell_insert.wav"
+})
+
+sound.Add({
+    name = "stevens620.shellinsertempty",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/stevens620/stevens620_shell_insert_empty.wav"
+})
+
+
+
+
+
+-- M1918 Tankgewehr
+sound.Add({
+    name = "tankgewehr.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/tankgewehr/arccw_t1918_shoot.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/tankgewehr/arccw_t1918_shoot_distance.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/tankgewehr/arccw_t1918_boltrelease.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/tankgewehr/arccw_t1918_boltback.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.bulletin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/tankgewehr/arccw_t1918_bulletin.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.boltforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/tankgewehr/arccw_t1918_boltforward.wav"
+})
+
+sound.Add({
+    name = "tankgewehr.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/tankgewehr/arccw_t1918_boltlatch.wav"
+})
+
+
+
+
+
+-- Volkssturmgewehr
+sound.Add({
+    name = "volkssturmgewehr.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = {"weapons/volkssturmgewehr/volkssturmgewehr_fire_1.wav", "weapons/volkssturmgewehr/volkssturmgewehr_fire_2.wav", "weapons/volkssturmgewehr/volkssturmgewehr_fire_3.wav",}
+})
+
+sound.Add({
+    name = "volkssturmgewehr.fire_suppressed",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/volkssturmgewehr/famas_suppressed_fire.wav"
+})
+
+
+sound.Add({
+    name = "volkssturmgewehr.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/volkssturmgewehr/famas_distance_fire.wav"
+})
+
+sound.Add({
+    name = "volkssturmgewehr.magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/volkssturmgewehr/volkssturmgewehr_magin.wav"
+})
+
+sound.Add({
+    name = "volkssturmgewehr.magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/volkssturmgewehr/volkssturmgewehr_magout.wav"
+})
+
+sound.Add({
+    name = "volkssturmgewehr.maghit",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/volkssturmgewehr/volkssturmgewehr_maghit.wav"
+})
+
+sound.Add({
+    name = "volkssturmgewehr.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/volkssturmgewehr/volkssturmgewehr_boltback.wav"
+})
+
+sound.Add({
+    name = "volkssturmgewehr.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/volkssturmgewehr/volkssturmgewehr_boltrelease.wav"
+})
+
+
+
+
+
+-- Wz 35
+sound.Add({
+    name = "blast_wz35.fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/wz35/arccw_wz35_shoot.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.fire_distant",
+    channel = CHAN_AUTO,
+    volume = 0.2,
+    level = 160,
+    sound = "weapons/wz35/arccw_wz35_shoot_distance.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_draw.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_boltrelease.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_boltback.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.boltforward",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_boltforward.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.boltlatch",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_boltlatch.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_magin.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_magout.wav"
+})
+
+sound.Add({
+    name = "blast_wz35.dryfire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/wz35/arccw_wz35_dryfire.wav"
+})
+
+
+
+
+
+-- 
